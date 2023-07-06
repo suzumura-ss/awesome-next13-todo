be7cee74ae95342ac65e773911d18e4423da5e87
Toshiyuki Suzumura <suz@smoche.info>
2023-07-06 19:58:51 +0900

    フロント側のjest testを通した

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,18 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { prisma } from '@src/db';
-import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import React from 'react';
-
-async function createTodo(data: FormData) {
-  'use server';
-  const title = data.get('title')?.valueOf();
-  if (typeof title !== 'string' || title.length === 0) {
-    throw new Error('Title is required');
-  }
-  await prisma.toDo.create({ data: { title, completed: false } });
-  redirect('/');
-}
+import { createTodo } from '@backend/todo';
 
 export default function Page() {
   return (
